5418f261ca79864c4117938fc50c44c6339d40c7
CHOI SUJIN <amor77777777778@gmail.com>
2018-10-28 16:08:33 +0900

    Updates

---
+++ precache-manifest.2199ef750090bb14e93f66d0a14c4679.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "1657ace312e4257b4f4d",
+    "url": "/todolist/static/js/main.1657ace3.chunk.js"
+  },
+  {
+    "revision": "6db6ff6a318fdf9d171d",
+    "url": "/todolist/static/js/1.6db6ff6a.chunk.js"
+  },
+  {
+    "revision": "1657ace312e4257b4f4d",
+    "url": "/todolist/static/css/main.e91c358a.chunk.css"
+  },
+  {
+    "revision": "7d0c21ba2d70166a70278294fc46f50a",
+    "url": "/todolist/index.html"
+  }
+];
